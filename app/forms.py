013598d8720e43814c78ff634f862d1d0a31ce67
Loner1024 <xth12138@gmail.com>
2019-02-24 01:37:41 +0800

    change

--- app/forms.py
+++ app/forms.py
@@ -3,5 +3,5 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class get_ical(FlaskForm):
-    student_id = StringField('学号：',validators=[DataRequired()])
+    student_id = StringField('学号：', validators=[DataRequired()])
     submit = SubmitField('获取')
